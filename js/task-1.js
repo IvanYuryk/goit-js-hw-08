3b2fb5528bfc0108e8dcd2b79b926e74e08211bb
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-01-15 17:02:35 +0100

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.getElementById('categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(categoryItem => {
+  const categoryName = categoryItem.querySelector('h2').textContent;
+  const categoryItemsCount = categoryItem.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItemsCount}`);
+});
+
